b7da38e112b0b9af4c87e34b0c53855581c116d3
Rob Taylor <rob.taylor543@gmail.com>
2019-02-20 17:26:14 -0500

    Just need text alignment and silly stars to show how much a number is

--- word_frequency.py
+++ word_frequency.py
@@ -3,13 +3,51 @@ STOP_WORDS = [
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
+import string
 
+def clean(file):
+    """Read in a string and remove special characters and stop words (keep spaces). Return the result as a string."""
+    file = file.casefold()
+    valid_chars = string.ascii_letters + string.whitespace + string.digits
+
+    new_file = ""
+    for char in file:
+        if char in valid_chars:
+            new_file += char
+
+    file = new_file
+    file = file.replace("\n", " ")
+    return file
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
 
+    with open(file, "r") as open_file:
+        file_string = open_file.read()
+        
+    clean_file = clean(file_string)
 
+    word_freq = {}
+    words = []
+
+    for word in clean_file.split(" "):
+        if word and word not in STOP_WORDS:
+            words.append(word)
+
+    for word in words:
+        if word in word_freq:
+            word_freq[word] += 1
+        else:
+            word_freq[word] = 1
+
+    sorted_values = sorted(word_freq.values(), reverse = True)
+    sorted_keys = sorted(word_freq, key = word_freq.__getitem__, reverse = True)
+
+    i = 0
+    for word in sorted_keys:
+        print (f"{word} | {sorted_values[i]}")
+        i+=1
+    
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
